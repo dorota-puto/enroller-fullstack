5e5ebd7a013e6d954ec7d1a08bfc76600a0e75ac
Dorota Puto <dootras@gmail.com>
2021-06-16 22:10:55 +0200

    User registration

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -11,7 +11,13 @@
       <meetings-page :username="authenticatedUsername"></meetings-page>
     </div>
     <div v-else>
-      <login-form @login="login($event)"></login-form>
+    	<button :class="registering ? 'button-outline': '' " @click="registering = false" >Loguję się</button>
+    	<button :class="registering ? '': 'button-outline' " @click="registering = true">Zarejestruję się</button>
+    	
+    <div v-if="errorMessage"class="alert-warning">{{ errorMessage }}</div>
+      <login-form v-if="!registering" @login="login($event)"></login-form>
+      <login-form v-else @login="register($event)" button-label="Zarejestruj sie"></login-form>
+      
     </div>
   </div>
 </template>
@@ -25,7 +31,9 @@
         components: {LoginForm, MeetingsPage},
         data() {
             return {
-                authenticatedUsername: ""
+                authenticatedUsername: "",
+                registering: false,
+                errorMessage: ""
             };
         },
         methods: {
@@ -34,7 +42,17 @@
             },
             logout() {
                 this.authenticatedUsername = '';
-            }
+            },
+            register(user) {
+            	this.errorMessage= "";
+ 				this.$http.post('participants', user)
+     				.then(response => {
+         			this.registering = false;
+     			})
+     			.catch(response => {
+         			this.errorMessage = "Nazwa użytkownika jest zajęta" 
+     			});
+			}
         }
     };
 </script>
@@ -47,6 +65,9 @@
 
   .logo {
     vertical-align: middle;
+  }
+  .alert-warning {
+  color: #9b4dca;  
   }
 </style>
 
